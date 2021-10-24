686bed651e8e413b2d4bfee69b7e742255252661
Safwan Shaheer <devorein00@gmail.com>
2021-10-24 18:01:24 +0600

    Added post route for consistently hashing and detecting shard server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+// @ts-ignore
+import ConsistentHash from 'consistent-hash';
+import crypto from 'crypto';
 import express from 'express';
 import { Client } from 'pg';
+
+const hr = new ConsistentHash();
+hr.add('5432');
+hr.add('5433');
+hr.add('5434');
 
 function createClient(port: number) {
 	return new Client({
@@ -13,9 +21,9 @@ function createClient(port: number) {
 
 // Spin up three clients
 const clients = {
-	5432: createClient(5432),
-	5433: createClient(5433),
-	5434: createClient(5434),
+	'5432': createClient(5432),
+	'5433': createClient(5433),
+	'5434': createClient(5434),
 };
 
 const app = express();
@@ -25,14 +33,37 @@ connect();
 async function connect() {
 	try {
 		// Establishing tcp connection to postgres containers
-		await clients[5432].connect();
-		await clients[5433].connect();
-		await clients[5434].connect();
+		await clients['5432'].connect();
+		await clients['5433'].connect();
+		await clients['5434'].connect();
 	} catch (err) {
 		console.log(err.message);
 	}
 }
 
-app.get('/', async (req, res) => {});
+app.get('/', async () => {});
 
-app.post('/', async (req, res) => {});
+app.post('/', async (req, res) => {
+	try {
+		const url = req.query.url as string;
+		// consistently hash to get port
+		const hash = crypto.createHash('sha256').update(url).digest('base64');
+		const urlId = hash.substring(0, 5);
+		const server: '5432' | '5433' | '5434' = hr.get(urlId);
+		await clients[server].query('INSERT INTO url_table(url, url_id) values($1, $2)', [url, urlId]);
+		res.send({
+			urlId,
+			url,
+			server,
+		});
+	} catch (err) {
+		res.send({
+			message: err.message,
+			status: 'error',
+		});
+	}
+});
+
+app.listen(4000, () => {
+	console.log(`Server listening on port 4000`);
+});
